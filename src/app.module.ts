1f7dd0c6b0049cddd09a9715bafd3cc5b24ec06b
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-06-17 10:13:39 -0400

    feat: add modified app module with other modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from './auth/auth.module';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { ScansModule } from './scans/scans.module';
 
 @Module(
     {
-        
-    
-  imports: [AuthModule]
+  imports: [AuthModule, ReportsModule, UsersModule, ScansModule]
 }
 )
 export class AppModule{}
