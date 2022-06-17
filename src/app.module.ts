4a94ca1cb72bd0125d4bc8756dcd309075211d7d
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-06-17 10:01:33 -0400

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AuthModule } from './auth/auth.module';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+@Module(
+    {
+        
+    
+  imports: [AuthModule]
+}
+)
+export class AppModule{}
