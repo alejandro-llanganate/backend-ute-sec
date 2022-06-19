c8aaf065e6686db839eb3840de85dda441f23166
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-06-19 09:06:34 -0400

    feat: add getAllUsers services

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { Controller, Get, Post, Put } from "@nestjs/common";
+import { UserService } from "../services/user.service";
 
 @Controller('/users')
 export class UserController{
 
+    constructor(
+        private readonly userService : UserService,
+    ){}
+
     @Get('/getAllUsers')
     getAllUsers(){
-        return 'getAllUsers'
+        return 'getAllUsers' + this.userService.getAllUsers();
     }
 
     @Get('/getUserById')
