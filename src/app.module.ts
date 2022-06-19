fa48d76d9588f7f7f52853ca2f7198824252131a
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-06-19 16:45:01 -0400

    feat: add docker compose & forRoot of typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { AuthModule } from './auth/auth.module';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 import { ScansModule } from './scans/scans.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module(
-    {
-  imports: [AuthModule, ReportsModule, UsersModule, ScansModule]
-}
+  {
+    imports: [
+      AuthModule,
+      ReportsModule,
+      UsersModule,
+      ScansModule,
+      TypeOrmModule.forRoot(
+        {
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'root',
+          password: '1R4T55SFF1',
+          database: 'my_db',
+          autoLoadEntities: true,
+          synchronize: true,
+        }
+      )
+    ]
+  }
 )
-export class AppModule{}
+export class AppModule { }
