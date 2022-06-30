e89de096c93cba5b681e720de32824925a2819a0
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-06-30 10:17:14 -0400

    feat: add users add swager

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { CreateUserDTO, UpdateUserDTO, UpdateUserPasswordDTO } from "../dto/user.dto";
 import { UserService } from "../services/user.service";
 
+@ApiTags('Usuarios')
 @Controller('/User')
 export class UserController {
 
@@ -9,26 +11,51 @@ export class UserController {
         private readonly _userService: UserService,
     ) { }
 
+    @ApiOperation(
+        {
+            summary: 'Lista de usuarios del sistema'
+        }
+    )
     @Get('/getAllUsers')
     async getAllUsers() {
         return await this._userService.getAllUsers();
     }
 
+    @ApiOperation(
+        {
+            summary: 'Método para obtener un usuario según su id'
+        }
+    )
     @Get('/getUserById/:id')
     async getUserById(@Param('id') id: string) {
         return await this._userService.getUserById(id);
     }
 
+    @ApiOperation(
+        {
+            summary: 'Método para crear un usuario'
+        }
+    )
     @Post('/createUser')
     async createUser(@Body() user: CreateUserDTO) {
         return await this._userService.createUser(user);
     }
 
+    @ApiOperation(
+        {
+            summary: 'Método para actualizar un usuario según su id'
+        }
+    )
     @Put('/updateUser/:id')
     async updateUser(@Param('id') id: string, @Body() user: UpdateUserDTO) {
         return await this._userService.updateUser(id, user);
     }
 
+    @ApiOperation(
+        {
+            summary: 'Método para actualizar la contraseña de un usuario'
+        }
+    )
     @Put('/updateUserPassword')
     async updateUserPassword(@Body() userPasswordAndId : UpdateUserPasswordDTO){
         return await this._userService.updateUserPassword(userPasswordAndId.id, userPasswordAndId.password);
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 
@@ -5,18 +6,22 @@ export class CreateUserDTO {
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly name : string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly lastname: string;
 
     @IsEmail()
     @IsNotEmpty()
+    @ApiProperty()
     readonly email: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly password: string;
 }
 
@@ -24,27 +29,33 @@ export class UpdateUserDTO {
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly name : string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly lastname: string;
 
     @IsEmail()
     @IsNotEmpty()
+    @ApiProperty()
     readonly email: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly password: string;
 }
 
 export class UpdateUserPasswordDTO {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly id: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     readonly password: string;    
 }
